c6b803f340b3c89b01b12c442e1aae49b449acc9
Cruikshank <Matthew.Cruikshank@garland-group.com>
2025-05-20 12:55:41 -0400

    Updated to end of day 27

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: [
+        "./resources/**/*.clade.php",
+        "./resources/**/*.js"
+    ],
+    theme: {
+        entend: {
+            colors: {
+                "black": "#060606"
+            }
+        },
+    },
+    plugins: [],
+}
