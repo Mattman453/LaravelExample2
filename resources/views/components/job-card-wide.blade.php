966625259a7586f28eae101eba15f85178bd3b1f
Cruikshank <Matthew.Cruikshank@garland-group.com>
2025-05-20 13:32:17 -0400

    Added missing file

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <img src="http://place-hold.it/42/42" alt="" class="rounded-xl">
+
+    <div>
+        <a href="#" class="self-start text-sm">Laracasts</a>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
